81f0cd5b028888a67af0f7c98e5abe29f84470a0
jenkins96 <adrianjnkns@gmail.com>
2021-03-22 23:21:56 +0000

    initial files

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<header>
+			<div className="l-pad">
+				<span className="logo">
+					<h1>
+						Foogle <span className="logo-sub">play</span>
+					</h1>
+				</span>
 			</div>
-		</nav>
+			<div className="topbar cf">
+				<span className="topbar-title l-sidebar l-pad">
+					<i className="fas fa-book fa-lg" />
+					<h2>Books</h2>
+				</span>
+			</div>
+			<div />
+		</header>
 	);
 };
---
+++ src/js/component/sideBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SideBar = () => {
+	return (
+		<div className="categories l-sidebar standout">
+			<ul>
+				<li>
+					<a href="">All</a>
+				</li>
+				<li>
+					<a href="">Science Fiction</a>
+				</li>
+				<li>
+					<a href="">Romance</a>
+				</li>
+				<li>
+					<a href="">Horror</a>
+				</li>
+				<li>
+					<a href="">Sports</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+export default SideBar;
